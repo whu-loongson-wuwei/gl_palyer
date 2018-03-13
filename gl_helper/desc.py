8640d9e1280d415d9ec4b0f84101336d3f21e3f1
wuwei <wwzhongzheng@gmail.com>
2018-03-13 15:08:22 +0800

    desc

--- gl_helper/desc.py
+++ gl_helper/desc.py
@@ -1,14 +1,27 @@
-for ind in len(ps):
-	p = ps[ind].split()
-	if p[-1].find('*') == 0 :
-	    stmt_d_1 = self.indent
-	    stmt_d_1 += 'size = sizeof({})'.format(p[-1])
-	    stmt_d_2 = self.indent
-	    stmt_d_2 += 'p_depth = 0'
-	    stmt_d_3 = self.indent
-	    stmt_d_3 = 'fwrite(dp,1,size,&{})'.format(p[-1])
-	elif ' '.join(p[-1]).find('*') == 0:
-	    stmt_d_1 = self.indent
-	    if 
-	    stmt_d_1 += 'size = sizeof({})'.format(p[-1])
+import os
+file = open("args.desc.txt",'r')
+args_dict_write = {}
+args_dict_read = {}
+lines = file.readlines();
+size = len(lines)
+func_name_pre = 'begin_read'
+args_dict_read[func_name_pre] = []
+for index in xrange(size):
+    line = lines[index]
+    ind = line.find(r'//gl')
+    if ind != -1:
+        func_name = line[ind+2:line.find('\r')]
+        if args_dict_write.has_key(func_name):
+            args_dict_read[func_name] = []
+            args_dict_read[func_name].append(index+1)
+            args_dict_write[func_name].append(index)
+            func_name_pre = func_name
+        else:
+            args_dict_write[func_name] = []
+            args_dict_read[func_name_pre].append(index)
+            args_dict_write[func_name].append(index+1)
+print args_dict_write
+print '\n\n'
+print args_dict_read
+
 
